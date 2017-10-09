28ae2a19e4f1b246bf0aaacd2417fca2ef226e5b
HViktorTsoi <cuijiahetony@163.com>
2017-10-09 22:09:47 +0800

    增加了全局logger 实现实时输出下载信息

--- Downloader.py
+++ Downloader.py
@@ -3,9 +3,11 @@
 By HViktorTsoi
 
 """
+import sys
 import threading
 import time
 from contextlib import closing
+
 import requests
 import requests.adapters
 
@@ -43,7 +45,7 @@ class Downloader:
         self.__content_size = 0
         self.__file_lock = threading.Lock()
         self.__downloaded_size = 0
-        self.__threads_info = {}
+        self.__threads_status = {}
 
         requests.adapters.DEFAULT_RETRIES = 2
 
@@ -90,41 +92,65 @@ class Downloader:
         headers = {
             "Range": "bytes={}-{}".format(page["start_pos"], page["end_pos"])
         }
-        page_size = page["end_pos"] - page["start_pos"]
         thread_name = threading.current_thread().name
-        print("{} Start Downloading:{}".format(thread_name, headers))
         # 初始化进程信息列表
-        self.__threads_info[thread_name] = {
-            "page_size": page_size,
+        self.__threads_status[thread_name] = {
+            "page_size": page["end_pos"] - page["start_pos"],
             "page": page,
+            "status": 0,
         }
-        # 以流的方式进行get请求
-        with closing(requests.get(
-            url=url,
-            headers=headers,
-            stream=True,
-            timeout=self.timeout
-        )) as response:
-            for data in response.iter_content(chunk_size=self.chunk_size):
-                # 向目标文件中写入数据块,此处需要同步锁
-                with self.__file_lock:
-                    # 查找文件写入位置
-                    file.seek(page["start_pos"])
-                    # 写入文件
-                    file.write(data)
-                    self.__downloaded_size += len(data)
-                print("|- {}  Download: {}KB / Chunk: {}KB | {}KB / {}KB".format(
-                    thread_name,
-                    (page_size - (page["end_pos"] - page["start_pos"])) / 1024,
-                    page_size / 1024,
-                    self.__downloaded_size / 1024,
-                    self.__content_size / 1024
-                ))
-                # 数据流每向前流动一次,将文件指针同时前移
-                page["start_pos"] += self.chunk_size
-                self.__threads_info[thread_name]["page"] = page
-        print(" ==> {} Finished.".format(thread_name))
-        print(self.__threads_info)
+        try:
+            # 以流的方式进行get请求
+            with closing(requests.get(
+                url=url,
+                headers=headers,
+                stream=True,
+                timeout=self.timeout
+            )) as response:
+                for data in response.iter_content(chunk_size=self.chunk_size):
+                    # 向目标文件中写入数据块,此处需要同步锁
+                    with self.__file_lock:
+                        # 查找文件写入位置
+                        file.seek(page["start_pos"])
+                        # 写入文件
+                        file.write(data)
+                        self.__downloaded_size += len(data)
+                    # 数据流每向前流动一次,将文件指针同时前移
+                    page["start_pos"] += len(data)
+                    self.__threads_status[thread_name]["page"] = page
+                    self.__log_status()
+        except Exception as e:
+            print(e, file=sys.stderr)
+            self.__threads_status[thread_name]["status"] = 1
+
+    def __log_status(self):
+        print("\033c")
+        print("文件元信息:\nURL: {}\n文件名:{}".format(
+            self.__threads_status["url"],
+            self.__threads_status["target_file"]
+        ))
+        print("下载中......")
+        for thread_name, thread_status in self.__threads_status.items():
+            if thread_name not in ("url", "target_file"):
+                page_size = thread_status["page_size"]
+                page = thread_status["page"]
+                status = thread_status["status"]
+                if status == 0:
+                    if page["start_pos"] < page["end_pos"]:
+                        print("|- {}  Downloaded: {}KB / Chunk: {}KB".format(
+                            thread_name,
+                            (page_size - (page["end_pos"] -
+                                          page["start_pos"])) / 1024,
+                            page_size / 1024,
+                        ))
+                    else:
+                        print("|=> {} Finished.".format(thread_name))
+                elif status == 1:
+                    print("|XXX {} Crushed.".format(thread_name))
+        print("Downloaded: {}KB / Total: {}KB".format(
+            self.__downloaded_size / 1024,
+            self.__content_size / 1024
+        ))
 
     def start(self, url, target_file, urlhandler=lambda u: u):
         """开始下载
@@ -139,6 +165,8 @@ class Downloader:
         # 记录下载开始时间
         start_time = time.time()
         self.__establish_connect(url)
+        self.__threads_status["url"] = url
+        self.__threads_status["target_file"] = target_file
         with open(target_file, "wb+") as file:
             for page in self.__page_dispatcher():
                 thd = threading.Thread(
@@ -150,12 +178,15 @@ class Downloader:
                 thd.join()
         # 记录下载总计用时
         span = time.time() - start_time
+        self.__threads_status = {}
         print("总计用时:{}s".format(span))
 
 
 if __name__ == '__main__':
-    downloader = Downloader()
+    downloader = Downloader(
+        threads_num=20
+    )
     downloader.start(
-        url="https://fpdownload.macromedia.com/pub/flashplayer/latest/help/install_flash_player_ppapi.exe",
-        target_file="/tmp/tmp.exe",
+        url="http://fjyd.sc.chinaz.com/Files/DownLoad/pic9/201709/bpic3616.rar",
+        target_file="/tmp/tmp.rar",
     )
